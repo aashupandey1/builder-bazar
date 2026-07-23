554cee2de27b2e755369fcafc896701e3b5405c8
aashupandey <00ashupandey11@gmail.com>
2026-07-23 18:27:31 +0530

    fix: accept and spread style prop in Skeleton component

--- client/src/components/common/Skeleton/Skeleton.jsx
+++ client/src/components/common/Skeleton/Skeleton.jsx
@@ -1,10 +1,10 @@
 import './Skeleton.css';
 
-const Skeleton = ({ width = '100%', height = '16px', radius = '8px', className = '' }) => {
+const Skeleton = ({ width = '100%', height = '16px', radius = '8px', className = '', style = {} }) => {
   return (
     <div
       className={`skeleton ${className}`}
-      style={{ width, height, borderRadius: radius }}
+      style={{ width, height, borderRadius: radius, ...style }}
     />
   );
 };
