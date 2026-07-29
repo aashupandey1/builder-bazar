eb9d5f26172c2779235944a2e6f94a400c7edb48
aashupandey <00ashupandey11@gmail.com>
2026-07-29 13:55:35 +0530

    feat: add Sentry error tracking to frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,5 +3,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import * as Sentry from '@sentry/react';
+
+Sentry.init({
+  dsn: 'https://82e0a23c2f566cae6209d48dd672895d@o4511817519333376.ingest.us.sentry.io/4511817529032704',
+  environment: import.meta.env.MODE,
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
